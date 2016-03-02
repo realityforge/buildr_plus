cf06e7f09a934eeaad2108728129610b69edecae
Peter Donald <peter@realityforge.org>
2016-03-02 13:17:04 +1100

    Prefer single quotes

--- lib/buildr_plus/patches/jdepend.rb
+++ lib/buildr_plus/patches/jdepend.rb
@@ -37,7 +37,7 @@ module Buildr
 
         args = []
         if output_file
-          args << "-file"
+          args << '-file'
           args << output_file
         end
         target_paths.each do |target_path|
