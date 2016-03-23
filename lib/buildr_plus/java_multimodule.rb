bb793c719c240a32c7f855c9d988091e79f2ae91
Peter Donald <peter@realityforge.org>
2016-03-23 14:41:41 +1100

    Invert test to ensure sync_model is created unless standlaone

--- lib/buildr_plus/java_multimodule.rb
+++ lib/buildr_plus/java_multimodule.rb
@@ -23,7 +23,7 @@ if BuildrPlus::FeatureManager.activated?(:shared)
   BuildrPlus::Roles.project('shared', :roles => [:shared], :parent => :container, :template => true, :description => 'Shared Components')
 end
 BuildrPlus::Roles.project('model', :roles => [:model], :parent => :container, :template => true, :description => 'Persistent Entities, Messages and Data Structures')
-if BuildrPlus::FeatureManager.activated?(:sync) && BuildrPlus::Sync.standalone?
+if BuildrPlus::FeatureManager.activated?(:sync) && !BuildrPlus::Sync.standalone?
   BuildrPlus::Roles.project('sync_model', :roles => [:sync_model], :parent => :container, :template => true, :description => 'Shared Model used to write External synchronization services')
 end
 BuildrPlus::Roles.project('model-qa-support', :roles => [:model_qa_support], :parent => :container, :template => true, :description => 'Model Test Infrastructure')
