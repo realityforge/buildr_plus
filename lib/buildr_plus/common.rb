a88616beb2436abe5f8f5734160194174e0a605f
Peter Donald <peter@realityforge.org>
2016-04-21 20:30:47 +1000

    Only change the example config file when multi database project and pg variant. (Should only come into effect in the ci)

--- lib/buildr_plus/common.rb
+++ lib/buildr_plus/common.rb
@@ -26,12 +26,6 @@ BuildrPlus::FeatureManager.activate_feature(:domgen) if BuildrPlus::Util.is_domg
 BuildrPlus::FeatureManager.activate_feature(:rptman) if BuildrPlus::Util.is_rptman_gem_present?
 BuildrPlus::FeatureManager.activate_feature(:sass) if BuildrPlus::Util.is_sass_gem_present?
 
-if BuildrPlus::FeatureManager.activated?(:dbt)
-  database_config = if !BuildrPlus::Db.is_multi_database_project? || BuildrPlus::Db.mssql?
-    'config/ci-database.yml'
-  else
-    # Assume that a multi database project defaults to sql server and has second yml for pg
-    'config/ci-pg-database.yml'
-  end
-  Dbt::Config.example_config_filename = database_config
+if BuildrPlus::FeatureManager.activated?(:dbt) && BuildrPlus::Db.is_multi_database_project? && BuildrPlus::Db.pgsql?
+  Dbt::Config.example_config_filename = 'config/ci-pg-database.yml'
 end
