a5e2a3f91e0f2963db2877b99f6834655dc98e99
Peter Donald <peter@realityforge.org>
2016-04-25 21:35:27 +1000

    Add check to ensure that if domgen facet is enabled the equivalent buildr_plus feature is enabled

--- lib/buildr_plus/features/domgen.rb
+++ lib/buildr_plus/features/domgen.rb
@@ -120,6 +120,9 @@ BuildrPlus::FeatureManager.feature(:domgen) do |f|
               if BuildrPlus::FeatureManager.activated?(buildr_plus_facet) && !r.facet_enabled?(domgen_facet)
                 raise "BuildrPlus feature '#{buildr_plus_facet}' requires that domgen facet '#{domgen_facet}' is enabled but it is not."
               end
+              if !BuildrPlus::FeatureManager.activated?(buildr_plus_facet) && r.facet_enabled?(domgen_facet)
+                raise "Domgen facet '#{domgen_facet}' requires that buildrPlus feature '#{buildr_plus_facet}' is enabled but it is not."
+              end
             end
           end
         end
