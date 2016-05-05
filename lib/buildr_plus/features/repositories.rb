19d18dadd9a2e728949fb4db3b216ff5491b162a
Peter Donald <peter@realityforge.org>
2016-05-05 16:52:03 +1000

    Only add the public repo to non fisg projects as they can rely on virtual repo

--- lib/buildr_plus/features/repositories.rb
+++ lib/buildr_plus/features/repositories.rb
@@ -16,8 +16,8 @@ BuildrPlus::FeatureManager.feature(:repositories) do |f|
   f.enhance(:ProjectExtension) do
     first_time do
       Buildr.repositories.remote.unshift(ENV['DOWNLOAD_REPO']) if ENV['DOWNLOAD_REPO']
-      Buildr.repositories.remote.unshift('https://stocksoftware.artifactoryonline.com/stocksoftware/public')
       if BuildrPlus::FeatureManager.activated?(:github)
+        Buildr.repositories.remote.unshift('https://stocksoftware.artifactoryonline.com/stocksoftware/public')
         Buildr.repositories.remote.unshift('http://repo1.maven.org/maven2')
         Buildr.repositories.remote.unshift('https://stocksoftware.artifactoryonline.com/stocksoftware/oss')
         if BuildrPlus::FeatureManager.activated?(:geolatte)
