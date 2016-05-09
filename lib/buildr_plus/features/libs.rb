6d27b1f46bdd7aa3cb3399dac88ec45937f63cec
Peter Donald <peter@realityforge.org>
2016-05-10 07:34:45 +1000

    Bump the version of glassfish

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -53,7 +53,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def glassfish_embedded
-      %w(fish.payara.extras:payara-embedded-all:jar:4.1.1.154)
+      %w(fish.payara.extras:payara-embedded-all:jar:4.1.1.162)
     end
 
     def eclipselink
