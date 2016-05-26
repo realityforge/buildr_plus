6b51462a3b0881def27e4561118a34a4eb22ffe7
Peter Donald <peter@realityforge.org>
2016-05-26 14:19:07 +1000

    Default to mixing in redfish configuration generated by domgen into base domain

--- lib/buildr_plus/features/redfish.rb
+++ lib/buildr_plus/features/redfish.rb
@@ -20,6 +20,13 @@ BuildrPlus::FeatureManager.feature(:redfish) do |f|
 
     after_define do |buildr_project|
       if buildr_project.ipr?
+
+        if BuildrPlus::FeatureManager.activated?(:domgen) && Redfish.domain_by_name?(buildr_project.name)
+          domain = Redfish.domain_by_name(buildr_project.name)
+          domain.pre_artifacts << buildr_project._("generated/domgen/#{buildr_project.name}/main/etc/#{buildr_project.name_as_class}.redfish.fragment.json")
+          buildr_project.task ":#{domain.task_prefix}:pre_build" => ["#{buildr_project.name}:domgen:#{buildr_project.name}"]
+        end
+
         unless BuildrPlus::Util.subprojects(buildr_project).any? { |p| p == "#{buildr_project.name}:domains" }
           buildr_project.instance_eval do
             desc 'Redfish Domain Definitions'
