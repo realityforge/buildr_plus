c77f4fb77bfdd009b4e5f81cf0c3de4ab856cd32
Peter Donald <peter@realityforge.org>
2016-06-10 10:21:01 +1000

    Update redfish feature to enable auto creation of local and docker domains from core domain definition

--- lib/buildr_plus/features/redfish.rb
+++ lib/buildr_plus/features/redfish.rb
@@ -12,7 +12,44 @@
 # limitations under the License.
 #
 
-BuildrPlus::FeatureManager.feature(:redfish) do |f|
+BuildrPlus::FeatureManager.feature(:redfish => [:config]) do |f|
+  f.enhance(:Config) do
+    attr_writer :local_domain
+
+    def local_domain?
+      @local_domain.nil? ? true : @local_domain
+    end
+
+    attr_writer :docker_domain
+
+    def docker_domain?
+      @docker_domain.nil? ? true : @docker_domain
+    end
+
+    def features
+      if @features.nil?
+        @features = []
+        @features << :jms if BuildrPlus::FeatureManager.activated?(:jms)
+      end
+      @features
+    end
+
+    def system_property(domain, key, value)
+      RedfishPlus.system_property(domain, key, value)
+      domain.docker_run_args << "--env=#{key}=#{value}" if domain.dockerize?
+    end
+
+    def configure_system_properties(domain, environment)
+      domain.environment_vars.keys.each do |key|
+        if !domain.data['system_properties'].key?(key) || domain.data['system_properties'][key] == 'UNSPECIFIED'
+          raise "Redfish domain with key #{domain.key} requires setting #{key} that is not specified in development configuration." unless environment.setting?(key)
+          value = environment.settings[key]
+          system_property(domain, key, value)
+        end
+      end
+    end
+  end
+
   f.enhance(:ProjectExtension) do
     first_time do
       require 'redfish'
@@ -20,10 +57,36 @@ BuildrPlus::FeatureManager.feature(:redfish) do |f|
 
     before_define do |buildr_project|
       if buildr_project.ipr?
-        if Redfish.domain_by_key?("local_#{buildr_project.name}")
-          Redfish::Config.default_domain_key = "local_#{buildr_project.name}"
-        elsif Redfish.domain_by_key?(buildr_project.name)
-          Redfish::Config.default_domain_key = buildr_project.name
+        raise "Attempting to configure redfish domain with key #{docker_domain_key} but no development configuration present" unless BuildrPlus::Config.application_config.environment_by_key?('development')
+        environment = BuildrPlus::Config.application_config.environment_by_key('development')
+
+        local_domain_key = "local_#{buildr_project.name}"
+        if BuildrPlus::Redfish.local_domain? && Redfish.domain_by_key?(buildr_project.name) && !Redfish.domain_by_key?(local_domain_key)
+          Redfish.domain(local_domain_key, :extends => buildr_project.name) do |domain|
+            RedfishPlus.setup_for_local_development(domain, :features => BuildrPlus::Redfish.features)
+            BuildrPlus::Redfish.configure_system_properties(domain, environment)
+          end
+          Redfish::Config.default_domain_key = local_domain_key
+        end
+
+        docker_domain_key = "docker_#{buildr_project.name}"
+        if BuildrPlus::Redfish.local_domain? && Redfish.domain_by_key?(buildr_project.name) && !Redfish.domain_by_key?(docker_domain_key)
+          Redfish.domain(docker_domain_key, :extends => buildr_project.name) do |domain|
+            RedfishPlus.setup_for_docker(domain, :features => BuildrPlus::Redfish.features)
+            RedfishPlus.deploy_application(domain, buildr_project.name, '/', "{{file:#{buildr_project.name}}}")
+
+            if BuildrPlus::FeatureManager.activated?(:jms)
+              raise "Redfish domain with key #{domain.key} requires broker configuration that is not specified in development configuration." unless environment.broker?
+              # These are required as otherwise the glassfish will fail either when the
+              # application is deployed or when the server is reloaded
+              BuildrPlus::Redfish.system_property(domain, 'OPENMQ_HOST', environment.broker.host.to_s)
+              BuildrPlus::Redfish.system_property(domain, 'OPENMQ_PORT', environment.broker.port.to_s)
+              BuildrPlus::Redfish.system_property(domain, 'OPENMQ_ADMIN_USERNAME', environment.broker.admin_username.to_s)
+              BuildrPlus::Redfish.system_property(domain, 'OPENMQ_ADMIN_PASSWORD', environment.broker.admin_password.to_s)
+            end
+
+            BuildrPlus::Redfish.configure_system_properties(domain, environment)
+          end
         end
       end
     end
