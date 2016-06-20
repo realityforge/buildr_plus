a6174b57883b5c8a3ea62da56fdaa2787526d4d9
Peter Donald <peter@realityforge.org>
2016-06-20 15:31:50 +1000

    Avoid using redfish constant if library not present

--- lib/buildr_plus/features/config.rb
+++ lib/buildr_plus/features/config.rb
@@ -193,7 +193,7 @@ BuildrPlus::FeatureManager.feature(:config) do |f|
 
     def populate_volume_configuration(environment)
       buildr_project = get_buildr_project.root_project
-      if Redfish.domain_by_key?(buildr_project.name)
+      if BuildrPlus::FeatureManager.activated?(:redfish) && Redfish.domain_by_key?(buildr_project.name)
         domain = Redfish.domain_by_key(buildr_project.name)
 
         domain.volume_requirements.keys.each do |key|
