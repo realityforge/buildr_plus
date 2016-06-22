f3e9be65fb6c3140c7df724ab02b5cfb9a4facb0
Peter Donald <peter@realityforge.org>
2016-06-23 09:56:42 +1000

    Don't resolve project immediately as breaks projects which change group mid-flight

--- lib/buildr_plus/features/roles.rb
+++ lib/buildr_plus/features/roles.rb
@@ -208,7 +208,6 @@ BuildrPlus::FeatureManager.feature(:roles) do |f|
               project.define p.name do
                 self.apply_roles!
               end
-              project(p.name)
             end
           end
         end
