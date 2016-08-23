3ca6020791f0f4a79064d6c3cbdde4ff28781a06
Peter Donald <peter@realityforge.org>
2016-08-23 16:08:33 +1000

    Run a clean before re-packaging otherwise buildr can get confused

--- lib/buildr_plus/features/jenkins.rb
+++ lib/buildr_plus/features/jenkins.rb
@@ -197,6 +197,7 @@ CONTENT
           content += <<CONTENT
 
   stage 'Package Pg'
+  #{buildr_command('clean')}
   #{buildr_command('ci:package_no_test', :pre_script => "export DB_TYPE=pg\nexport TEST=no")}
 CONTENT
         end
