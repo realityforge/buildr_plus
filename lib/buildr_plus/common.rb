7d8c29d5c62f26336a83855fe0c164b609171830
Peter Donald <peter@realityforge.org>
2016-10-26 15:46:54 +1100

    Only enable :dialect_mapping if dbt enabled

--- lib/buildr_plus/common.rb
+++ lib/buildr_plus/common.rb
@@ -19,9 +19,10 @@ require 'buildr/single_intermediate_layout'
 require 'buildr/git_auto_version'
 require 'buildr/top_level_generate_dir'
 
-BuildrPlus::FeatureManager.activate_features([:repositories, :artifacts, :idea, :idea_codestyle, :roles, :product_version, :dialect_mapping, :libs, :deps, :publish, :whitespace, :gitignore, :artifact_assets, :ci, :config])
+BuildrPlus::FeatureManager.activate_features([:repositories, :artifacts, :idea, :idea_codestyle, :roles, :product_version, :libs, :deps, :publish, :whitespace, :gitignore, :artifact_assets, :ci, :config])
 
 BuildrPlus::FeatureManager.activate_feature(:dbt) if BuildrPlus::Util.is_dbt_gem_present?
+BuildrPlus::FeatureManager.activate_feature(:dialect_mapping) if BuildrPlus::FeatureManager.feature?(:dbt)
 BuildrPlus::FeatureManager.activate_feature(:domgen) if BuildrPlus::Util.is_domgen_gem_present?
 BuildrPlus::FeatureManager.activate_feature(:rptman) if BuildrPlus::Util.is_rptman_gem_present?
 BuildrPlus::FeatureManager.activate_feature(:sass) if BuildrPlus::Util.is_sass_gem_present?
