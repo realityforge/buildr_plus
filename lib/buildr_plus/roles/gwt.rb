329c53da8273dac3dc983b0c1be7bcc15f4b129f
Peter Donald <peter@realityforge.org>
2016-10-27 15:09:24 +1100

    Make sure model generates in model role

--- lib/buildr_plus/roles/gwt.rb
+++ lib/buildr_plus/roles/gwt.rb
@@ -18,7 +18,7 @@ BuildrPlus::Roles.role(:gwt) do
   project.publish = BuildrPlus::Artifacts.gwt?
 
   if BuildrPlus::FeatureManager.activated?(:domgen)
-    generators = [:gwt, :gwt_rpc_shared, :gwt_rpc_client_service, :gwt_client_jso, :auto_bean, :gwt_client_module]
+    generators = [:gwt, :gwt_rpc_shared, :gwt_rpc_client_service, :gwt_client_jso, :auto_bean, :gwt_client_module, :gwt_client_gwt_model_module]
     generators += [:keycloak_gwt_jso] if BuildrPlus::FeatureManager.activated?(:keycloak)
     generators += [:imit_shared, :imit_client_service, :imit_client_entity] if BuildrPlus::FeatureManager.activated?(:replicant)
     generators += project.additional_domgen_generators
