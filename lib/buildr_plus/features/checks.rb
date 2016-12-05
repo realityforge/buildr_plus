3f155826f0cf7e58cc5f10dfabb279ff1d7dad0b
Peter Donald <peter@realityforge.org>
2016-12-05 12:04:35 +1100

    Check that gwt_cache_filter and the library role do not co-exist

--- lib/buildr_plus/features/checks.rb
+++ lib/buildr_plus/features/checks.rb
@@ -27,6 +27,9 @@ BuildrPlus::FeatureManager.feature(:checks) do |f|
       if BuildrPlus::FeatureManager.activated?(:appcache) && BuildrPlus::FeatureManager.activated?(:role_library)
         raise "Can not enable the BuildrPlus 'appcache' feature for libraries"
       end
+      if BuildrPlus::FeatureManager.activated?(:gwt_cache_filter) && BuildrPlus::FeatureManager.activated?(:role_library)
+        raise "Can not enable the BuildrPlus 'gwt_cache_filter' feature for libraries"
+      end
     end
 
     desc 'Apply basic fixes on formats of local files'
