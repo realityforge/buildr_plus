4559f6a576fe53628f5be03a75878c9dd32dfafd
Peter Donald <peter@realityforge.org>
2016-12-12 20:10:52 +1100

    Increase the permgen for superdev mode

--- lib/buildr_plus/roles/container.rb
+++ lib/buildr_plus/roles/container.rb
@@ -129,7 +129,7 @@ BuildrPlus::Roles.role(:container) do
         path = "#{Reality::Naming.underscore(path)}.html" if path.size > 0
         ipr.add_gwt_configuration(p,
                                   :gwt_module => gwt_module,
-                                  :vm_parameters => '-Xmx3G',
+                                  :vm_parameters => '-Xmx3G -XX:MaxPermSize=512m',
                                   :shell_parameters => "-port 8888 -codeServerPort 8889 -bindAddress 0.0.0.0 -war #{_(:generated, 'gwt-export')}/",
                                   :launch_page => "http://127.0.0.1:8080/#{p.root_project.name}/#{path}")
       end
