96ebe5b23ee17d2100e40f0892d42de973d346de
Peter Donald <peter@realityforge.org>
2017-01-24 16:55:02 +1100

    Revert "Bump the version of lognice in use"
    This reverts commit 14d1f5df9dd0a8e5f0f406778b0fa102c54726c2.

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -125,7 +125,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def gwt_lognice
-      %w(org.realityforge.gwt.lognice:gwt-lognice:jar:0.2)
+      %w(org.realityforge.gwt.lognice:gwt-lognice:jar:0.4)
     end
 
     def gwt_appcache_client
