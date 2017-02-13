edad4585884a9fcc900bd3d6c90453942f56db79
Peter Donald <peter@realityforge.org>
2017-02-13 16:31:17 +1100

    Bump guiceyloops version

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -259,7 +259,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def guiceyloops_lib
-      'org.realityforge.guiceyloops:guiceyloops:jar:0.82'
+      'org.realityforge.guiceyloops:guiceyloops:jar:0.83'
     end
 
     def guiceyloops_gwt
