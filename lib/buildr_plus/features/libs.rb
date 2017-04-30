c3feecb12d3545612ba3d4a431d9c933e483d86d
Peter Donald <peter@realityforge.org>
2017-05-01 09:42:45 +1000

    Upgrade gwt-webpoller

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -136,7 +136,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def gwt_webpoller
-      %w(org.realityforge.gwt.webpoller:gwt-webpoller:jar:0.9.3)
+      %w(org.realityforge.gwt.webpoller:gwt-webpoller:jar:0.9.5)
     end
 
     def gwt_datatypes
