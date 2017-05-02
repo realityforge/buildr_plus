189111dd6252da5aefec26b011d7078fce732186
Peter Donald <peter@realityforge.org>
2017-05-02 16:24:39 +1000

    Make sure the idea codestyle aligns annotations on local vars as expected

--- lib/buildr_plus/features/idea_codestyle.rb
+++ lib/buildr_plus/features/idea_codestyle.rb
@@ -15,7 +15,7 @@
 BuildrPlus::FeatureManager.feature(:idea_codestyle) do |f|
   f.enhance(:Config) do
     def default_codestyle
-      'au.com.stocksoftware.idea.codestyle:idea-codestyle:xml:1.6'
+      'au.com.stocksoftware.idea.codestyle:idea-codestyle:xml:1.7'
     end
 
     def codestyle
