2f0253789f1902ee9cfde1cfdfdb19094f3b438d
Peter Donald <peter@realityforge.org>
2017-07-14 15:46:05 +1000

    Update to the next version of lognice

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -156,7 +156,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def gwt_lognice
-      %w(org.realityforge.gwt.lognice:gwt-lognice:jar:0.5)
+      %w(org.realityforge.gwt.lognice:gwt-lognice:jar:0.6)
     end
 
     def gwt_appcache_client
