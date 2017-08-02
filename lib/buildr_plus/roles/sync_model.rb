7b152ac1f79e005213c6f7d284114b3e451f4ce6
Peter Donald <peter@realityforge.org>
2017-08-02 12:43:59 +1000

    Generate a remote sync service when sync is not standalone

--- lib/buildr_plus/roles/sync_model.rb
+++ lib/buildr_plus/roles/sync_model.rb
@@ -15,7 +15,7 @@
 BuildrPlus::Roles.role(:sync_model, :requires => [:sync]) do
 
   if BuildrPlus::FeatureManager.activated?(:domgen)
-    generators = [:sync_master_ejb_impl, :ejb_services]
+    generators = [:sync_master_ejb_impl, :ejb_services, :sync_remote_sync_service]
     generators += project.additional_domgen_generators
     Domgen::Build.define_generate_task(generators, :buildr_project => project) do |t|
       t.filter = Proc.new do |artifact_type, artifact|
