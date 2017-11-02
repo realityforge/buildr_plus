1514b34e45f8bf34fe265ebe4d64e17a497711b7
Peter Donald <peter@realityforge.org>
2017-11-02 14:45:39 +1100

    Remove unnecessary configuration

--- lib/buildr_plus/features/arez.rb
+++ lib/buildr_plus/features/arez.rb
@@ -17,10 +17,7 @@ BuildrPlus::FeatureManager.feature(:arez) do |f|
 
     def arez_test_options
       {
-        'braincheck.dynamic_provider' => 'true',
         'braincheck.environment' => 'development',
-        'arez.dynamic_provider' => 'true',
-        'arez.logger' => 'proxy',
         'arez.environment' => 'development'
       }
     end
