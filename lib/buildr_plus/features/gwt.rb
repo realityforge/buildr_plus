69f98291fa3b6bf5ddc6f5742bb655451de0a19c
Peter Donald <peter@realityforge.org>
2018-02-12 15:45:08 +1100

    Patch gwt addon

--- lib/buildr_plus/features/gwt.rb
+++ lib/buildr_plus/features/gwt.rb
@@ -83,7 +83,7 @@ BuildrPlus::FeatureManager.feature(:gwt => [:jackson, :javascript]) do |f|
 
   f.enhance(:ProjectExtension) do
     first_time do
-      require 'buildr/gwt'
+      require 'buildr_plus/patches/gwt_patch'
     end
 
     def top_level_gwt_modules
