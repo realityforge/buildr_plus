6f7af5a48936f2433a264e2992b9cb7a0a49ca8d
Peter Donald <peter@realityforge.org>
2018-04-26 14:01:32 +1000

    Make sure the IDEA projects do not include deps transitively available

--- lib/buildr_plus/features/idea.rb
+++ lib/buildr_plus/features/idea.rb
@@ -23,6 +23,19 @@ BuildrPlus::FeatureManager.feature(:idea) do |f|
 
   f.enhance(:ProjectExtension) do
     after_define do |project|
+      if project.iml?
+        project_deps = Buildr.artifacts([project.iml.main_dependencies]).collect do |d|
+          Buildr.projects(:no_invoke => true).select do |other_project|
+            [other_project.packages, other_project.compile.target, other_project.resources.target, other_project.test.compile.target, other_project.test.resources.target].flatten.
+              detect {|artifact| artifact.to_s == d.to_s}
+          end
+        end.flatten
+
+        project.iml.main_dependencies.delete_if do |candidate|
+          project_deps.any? {|p| Buildr.artifacts([p.compile.dependencies]).any? {|artifact| candidate.to_s == artifact.to_s}}
+        end
+      end
+
       if project.ipr?
         project.iml.excluded_directories << project._(:artifacts)
 
