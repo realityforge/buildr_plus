339c1dd060aa38317bce28a8e341c97caf50d264
Peter Donald <peter@realityforge.org>
2018-05-31 17:30:45 +1000

    Avoid referencing generator that no longer exists

--- lib/buildr_plus/features/deps.rb
+++ lib/buildr_plus/features/deps.rb
@@ -156,7 +156,7 @@ BuildrPlus::FeatureManager.feature(:deps => [:libs]) do |f|
     def gwt_generators
       generators = [:ce_data_types, :gwt_client_config, :gwt, :gwt_rpc_shared, :gwt_rpc_client_service, :gwt_client_jso, :gwt_client_module, :gwt_client_gwt_model_module]
       generators += [:keycloak_gwt_jso] if BuildrPlus::FeatureManager.activated?(:keycloak)
-      generators += [:arez_entity, :arez_client_dao_gwt] if BuildrPlus::FeatureManager.activated?(:arez)
+      generators += [:arez_entity] if BuildrPlus::FeatureManager.activated?(:arez)
       generators += [:imit_shared, :imit_client_entity, :ce_data_types, :imit_client_entity_gwt_module, :imit_client_entity_gwt, :imit_client_service] if BuildrPlus::FeatureManager.activated?(:replicant)
 
       generators += self.shared_generators unless BuildrPlus::FeatureManager.activated?(:role_shared)
