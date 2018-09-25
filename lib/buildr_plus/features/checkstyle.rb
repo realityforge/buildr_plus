da657e6ef28bb5490a855fcec02a7593813dad75
Peter Donald <peter@realityforge.org>
2018-09-25 10:25:45 +1000

    Add rule to ensure xml Action annotation does not creep back into code-base

--- lib/buildr_plus/features/checkstyle.rb
+++ lib/buildr_plus/features/checkstyle.rb
@@ -220,6 +220,10 @@ BuildrPlus::FeatureManager.feature(:checkstyle) do |f|
         r.subpackage_rule('client', "#{g}.client")
         r.subpackage_rule('client.ioc', 'javax.inject')
 
+        if BuildrPlus::FeatureManager.activated?(:arez)
+          r.subpackage_rule('client', 'javax.xml.ws.Action', :rule_type => :class)
+        end
+
         if BuildrPlus::FeatureManager.activated?(:berk)
           r.subpackage_rule('client', 'iris.berk.server.data_type.EnvironmentSettingDTO', :rule_type => :class, :disallow => true)
         end
