5e112bac27848af8f707370b03b8daf8f94a7f79
Peter Donald <peter@realityforge.org>
2018-11-21 21:54:44 +1100

    Start to automate the filtering out of less useful spotbugs errors

--- lib/buildr_plus/features/spotbugs.rb
+++ lib/buildr_plus/features/spotbugs.rb
@@ -12,6 +12,83 @@
 # limitations under the License.
 #
 
+module BuildrPlus::Spotbugs
+  class FilterRule
+    def initialize(bug_pattern, options = {})
+      @bug_pattern = bug_pattern
+      @class_name_pattern = options[:class_name_pattern]
+    end
+
+    attr_reader :bug_pattern
+    attr_reader :class_name_pattern
+
+    def as_xml(indent = 1)
+      xml = "#{'  ' * indent}<Match>\n"
+      xml += "#{'  ' * (indent + 1)}<Class name=\"#{self.class_name_pattern}\"/>\n" if self.class_name_pattern
+      xml += "#{'  ' * (indent + 1)}<Bug pattern=\"#{self.bug_pattern}\"/>\n"
+      xml + "#{'  ' * indent}</Match>\n"
+    end
+  end
+
+  class Config
+    def initialize
+      @rules = []
+    end
+
+    def rule(bug_pattern, options = {})
+      @rules << FilterRule.new(bug_pattern, options)
+    end
+
+    def rules
+      @rules.dup
+    end
+
+    def as_xml
+      "<FindBugsFilter>\n#{rules_as_xml}</FindBugsFilter>\n"
+    end
+
+    def rules_as_xml
+      xml = ''
+      rules.each do |r|
+        xml += r.as_xml
+      end
+      xml
+    end
+  end
+
+  def self.setup_filter_config
+    config = Config.new
+
+    config.rule('FCCD_FIND_CLASS_CIRCULAR_DEPENDENCY')
+    config.rule('ITC_INHERITANCE_TYPE_CHECKING')
+    config.rule('PCAIL_POSSIBLE_CONSTANT_ALLOCATION_IN_LOOP')
+    config.rule('BL_BURYING_LOGIC')
+    if BuildrPlus::FeatureManager.activated?(:arez)
+      config.rule('CLI_CONSTANT_LIST_INDEX,IICU_INCORRECT_INTERNAL_CLASS_USE,CC_CYCLOMATIC_COMPLEXITY,EXS_EXCEPTION_SOFTENING_NO_CHECKED,PME_POOR_MANS_ENUM,PRMC_POSSIBLY_REDUNDANT_METHOD_CALLS,EI_EXPOSE_REP,RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE',
+                  :class_name_pattern => '~.*\.Arez_.*')
+    end
+
+    if BuildrPlus::FeatureManager.activated?(:react4j)
+      config.rule('ACEM_ABSTRACT_CLASS_EMPTY_METHODS,CE_CLASS_ENVY',
+                  :class_name_pattern => '~.*\.React4j_.*')
+    end
+
+    # gwt facet is a proxy for dagger
+    if BuildrPlus::FeatureManager.activated?(:gwt)
+      config.rule('IICU_INCORRECT_INTERNAL_CLASS_USE,CE_CLASS_ENVY',
+                  :class_name_pattern => '~.*_Factory')
+      config.rule('IICU_INCORRECT_INTERNAL_CLASS_USE',
+                  :class_name_pattern => '~.*DaggerModule_.*')
+      config.rule('IICU_INCORRECT_INTERNAL_CLASS_USE,UP_UNUSED_PARAMETER',
+                  :class_name_pattern => '~.*\.Dagger.*DaggerComponent')
+      config.rule('IICU_INCORRECT_INTERNAL_CLASS_USE',
+                  :class_name_pattern => '~.*\.Dagger.*DaggerComponent\$Builder')
+    end
+
+    config
+  end
+
+end
 BuildrPlus::FeatureManager.feature(:spotbugs) do |f|
   f.enhance(:Config) do
     attr_accessor :additional_project_names
@@ -26,6 +103,31 @@ BuildrPlus::FeatureManager.feature(:spotbugs) do |f|
       if project.ipr?
         project.spotbugs.enabled = true
         project.spotbugs.config_directory = project._(:etc, :spotbugs)
+
+        target_filters_file = project._(:target, :generated, :spotbugs, 'filter.xml')
+
+        t = task 'spotbugs:setup' do
+          FileUtils.mkdir_p File.dirname(target_filters_file)
+
+          config = BuildrPlus::Spotbugs.setup_filter_config
+          if File.exist?(project.spotbugs.filter_file)
+            content = IO.read(project.spotbugs.filter_file)
+            content.gsub!('</FindBugsFilter>', config.rules_as_xml)
+            IO.write(target_filters_file, content)
+          else
+            IO.write(target_filters_file, config.as_xml)
+          end
+          project.spotbugs.filter_file = target_filters_file
+        end
+
+        task 'spotbugs:xml' => %w(spotbugs:setup)
+        task 'spotbugs:html' => %w(spotbugs:setup)
+
+        project.task(':domgen:all').enhance([t.name])
+
+        project.clean do
+          FileUtils.rm_rf project._(:target, :generated, :spotbugs)
+        end
       end
     end
 
