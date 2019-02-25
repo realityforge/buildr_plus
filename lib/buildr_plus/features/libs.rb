d545f1238a05637f8bd10a4ef2c67d79f53e0b4b
Peter Donald <peter@realityforge.org>
2019-02-25 11:47:43 +1100

    Upgrade braincheck

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -97,7 +97,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def braincheck
-      %w(org.realityforge.braincheck:braincheck:jar:1.12.0)
+      %w(org.realityforge.braincheck:braincheck:jar:1.13.0)
     end
 
     def jsinterop
