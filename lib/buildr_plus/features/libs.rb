a7afe97c01731c40b7126887767fe1a94f2fedd1
Peter Donald <peter@realityforge.org>
2019-02-25 11:48:56 +1100

    Upgrade Elemental2

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -109,7 +109,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def elemental2_version
-      '1.0.0-b17-6897368'
+      '1.0.0-b18-f3472e7'
     end
 
     def elemental2_group_id
