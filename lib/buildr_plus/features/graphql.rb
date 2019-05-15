db03672cfd5caa933d912b381db163803f8cfeac
Peter Donald <peter@realityforge.org>
2019-05-15 10:12:42 +1000

    Package up the aggregated graphqls SDL document as part of release

--- lib/buildr_plus/features/graphql.rb
+++ lib/buildr_plus/features/graphql.rb
@@ -12,4 +12,43 @@
 # limitations under the License.
 #
 
-BuildrPlus::FeatureManager.feature(:graphql)
+BuildrPlus::FeatureManager.feature(:graphql) do |f|
+  f.enhance(:ProjectExtension) do
+    after_define do |buildr_project|
+      if buildr_project.ipr?
+
+        desc 'GraphQL Schema'
+        define :graphqls do
+          project.no_iml
+
+          [:graphqls, :graphqls_sources].each do |type|
+            package(type).enhance do |t|
+              project.task(':domgen:all').invoke
+              mkdir_p File.dirname(t.to_s)
+              content = IO.read(root_project._("server/generated/domgen/server/main/java/#{project.group.gsub('.', '/')}/server/#{root_project.name}.graphqls"))
+              Dir["#{root_project._("server/src/main/java/#{project.group.gsub('.', '/')}")}/**/*.graphqls"].each do |f|
+                content += IO.read(f)
+              end
+
+              IO.write(t.to_s, content)
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+class Buildr::Project
+  def package_as_graphqls(file_name)
+    file(file_name)
+  end
+
+  def package_as_graphqls_sources_spec(spec)
+    spec.merge(:type => :graphqls, :classifier => :sources)
+  end
+
+  def package_as_graphqls_sources(file_name)
+    file(file_name)
+  end
+end
