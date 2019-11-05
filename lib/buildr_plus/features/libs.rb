faf85e31b15ab5b5361ba3ac700bb099390b1df4
Peter Donald <peter@realityforge.org>
2019-11-05 20:48:49 +1100

    Bump some arez versions

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -283,7 +283,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def arez_version
-      '0.152'
+      '0.153'
     end
 
     def arez
@@ -295,7 +295,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def arez_spytools
-      %w(org.realityforge.arez.spytools:arez-spytools:jar:0.78)
+      %w(org.realityforge.arez.spytools:arez-spytools:jar:0.79)
     end
 
     def grim_annotations
