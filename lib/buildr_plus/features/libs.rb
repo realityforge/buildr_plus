94399da1abe29d73bf4ec0fbefddcc063e9d85e0
Peter Donald <peter@realityforge.org>
2019-11-05 12:28:59 +1100

    Update spytools

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -295,7 +295,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def arez_spytools
-      %w(org.realityforge.arez.spytools:arez-spytools:jar:0.77)
+      %w(org.realityforge.arez.spytools:arez-spytools:jar:0.78)
     end
 
     def grim_annotations
