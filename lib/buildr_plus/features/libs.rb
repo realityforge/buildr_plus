d04c114b2507bc56b4f745ad93736fb69fdc3a06
Peter Donald <peter@realityforge.org>
2019-11-12 10:56:53 +1100

    Move to the latest version of react4j/elemental2

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -114,7 +114,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def elemental2_version
-      '2.25'
+      '2.27'
     end
 
     def elemental2_group_id
@@ -295,7 +295,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def arez_spytools
-      %w(org.realityforge.arez.spytools:arez-spytools:jar:0.80)
+      %w(org.realityforge.arez.spytools:arez-spytools:jar:0.81)
     end
 
     def grim_annotations
@@ -315,7 +315,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def react4j_version
-      '0.140'
+      '0.141'
     end
 
     def react4j
