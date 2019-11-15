1dd26ef425959df5df9967aefabf7263374bb9d3
Peter Donald <peter@realityforge.org>
2019-11-15 11:13:05 +1100

    Only skip ffm repository for opensource libraries

--- lib/buildr_plus/features/repositories.rb
+++ lib/buildr_plus/features/repositories.rb
@@ -25,7 +25,7 @@ BuildrPlus::FeatureManager.feature(:repositories) do |f|
       if BuildrPlus::FeatureManager.activated?(:geolatte)
         Buildr.repositories.remote.unshift('http://download.osgeo.org/webdav/geotools')
       end
-      unless BuildrPlus::FeatureManager.activated?(:github)
+      unless BuildrPlus::FeatureManager.activated?(:oss)
         Buildr.repositories.remote.unshift('http://repo.ffm.vic.gov.au/repository/ffm')
       end
     end
