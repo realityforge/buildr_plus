86e14ba302a7c932f654c3d65a5dc5c233e2ab53
Peter Donald <peter@realityforge.org>
2019-12-04 11:28:41 +1100

    Upgrade the codestyle in use

--- lib/buildr_plus/features/idea_codestyle.rb
+++ lib/buildr_plus/features/idea_codestyle.rb
@@ -15,7 +15,7 @@
 BuildrPlus::FeatureManager.feature(:idea_codestyle) do |f|
   f.enhance(:Config) do
     def default_codestyle
-      'au.com.stocksoftware.idea.codestyle:idea-codestyle:xml:1.15'
+      'au.com.stocksoftware.idea.codestyle:idea-codestyle:xml:1.16'
     end
 
     def codestyle
