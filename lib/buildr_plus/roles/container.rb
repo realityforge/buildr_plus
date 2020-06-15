d1b5c5ac8a85956f3b02c402876b95a47605c260
Peter Donald <peter@realityforge.org>
2020-06-16 09:07:37 +1000

    Revert to noincremental as we are seeing to many gwt errors relating to stale code

--- lib/buildr_plus/roles/container.rb
+++ lib/buildr_plus/roles/container.rb
@@ -125,7 +125,7 @@ BuildrPlus::Roles.role(:container) do
                                   :start_javascript_debugger => false,
                                   :open_in_browser => false,
                                   :vm_parameters => '-Xmx3G',
-                                  :shell_parameters => "#{BuildrPlus::Gwt.enable_gwt_js_exports? ? '-generateJsInteropExports ' : ''}-codeServerPort #{BuildrPlus::Gwt.code_server_port} -bindAddress 0.0.0.0 -deploy #{_(:generated, :gwt, 'deploy')} -extra #{_(:generated, :gwt, 'extra')} -war #{_(:generated, :gwt, 'war')} -style PRETTY -XmethodNameDisplayMode FULL -incremental -logLevel INFO -strict -nostartServer",
+                                  :shell_parameters => "#{BuildrPlus::Gwt.enable_gwt_js_exports? ? '-generateJsInteropExports ' : ''}-codeServerPort #{BuildrPlus::Gwt.code_server_port} -bindAddress 0.0.0.0 -deploy #{_(:generated, :gwt, 'deploy')} -extra #{_(:generated, :gwt, 'extra')} -war #{_(:generated, :gwt, 'war')} -style PRETTY -XmethodNameDisplayMode FULL -noincremental -logLevel INFO -strict -nostartServer",
                                   :launch_page => "http://127.0.0.1:8080/#{p.root_project.name}/#{path}")
       end
     end
