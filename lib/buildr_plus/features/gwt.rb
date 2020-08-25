4ca520d1f928a015bf7dae9228e4fb1bd0954e19
Peter Donald <peter@realityforge.org>
2020-08-25 11:37:16 +1000

    Turn off file watching which sometimes locks up under OSX

--- lib/buildr_plus/features/gwt.rb
+++ lib/buildr_plus/features/gwt.rb
@@ -31,7 +31,7 @@ BuildrPlus::FeatureManager.feature(:gwt => [:sting, :jackson, :javascript]) do |
     end
 
     def gwtc_java_args
-      %w(-ea -Djava.awt.headless=true -Xms512M -Xmx3G)
+      %w(-ea -Djava.awt.headless=true -Dgwt.watchFileChanges=false -Xms512M -Xmx3G)
     end
 
     def add_source_to_jar(project)
