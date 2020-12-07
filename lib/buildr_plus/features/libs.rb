c7d6921a9934685ac96bf9847fddd500e5f6261e
Peter Donald <peter@realityforge.org>
2020-12-08 10:37:24 +1100

    Bump the version of guiceyloops

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -402,7 +402,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def guiceyloops
-      self.mockito + self.testng + %w(org.realityforge.guiceyloops:guiceyloops:jar:0.106) + self.guice + self.glassfish_embedded
+      self.mockito + self.testng + %w(org.realityforge.guiceyloops:guiceyloops:jar:0.107) + self.guice + self.glassfish_embedded
     end
 
     def glassfish_timers_domain
