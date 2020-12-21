0776fd3ce028cf020b94312f8e580824e1c0e31b
Peter Donald <peter@realityforge.org>
2020-12-21 17:33:47 +1100

    Bump the version of giggle

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -16,7 +16,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
   f.enhance(:Config) do
 
     def giggle
-      'org.realityforge.giggle:giggle-compiler:jar:all:0.12'
+      'org.realityforge.giggle:giggle-compiler:jar:all:0.13'
     end
 
     def mustache
