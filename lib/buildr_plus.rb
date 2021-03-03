68801246a97a966ff48db5e0c71a8ddaf5b89ffd
Peter Donald <peter@realityforge.org>
2021-03-03 13:25:16 +1100

    Remove patch applied upstream

--- lib/buildr_plus.rb
+++ lib/buildr_plus.rb
@@ -35,7 +35,6 @@ require 'buildr_plus/util'
 # Patches that should always be applied
 require 'buildr_plus/patches/group_project_patch'
 require 'buildr_plus/patches/resources_filter_patch'
-require 'buildr_plus/patches/warn_patch'
 
 require 'buildr_plus/features/appconfig'
 require 'buildr_plus/features/arez'
