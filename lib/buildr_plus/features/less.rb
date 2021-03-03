7e194f3cc73c1d3e093d1968a31471a20cb1cf98
Peter Donald <peter@realityforge.org>
2021-03-03 13:22:19 +1100

    Merge patch inline to feature

--- lib/buildr_plus/features/less.rb
+++ lib/buildr_plus/features/less.rb
@@ -27,6 +27,59 @@ BuildrPlus::FeatureManager.feature(:less) do |f|
     def less_path
       options[:source_dir] || BuildrPlus::Less.default_less_path
     end
+
+    def define_lessc_task(project, options = {})
+      params = {
+        :js => false,
+        :strict_math => true,
+        :optimize => true,
+        :strict_units => true,
+        :target_dir => project._(:generated, :less, :main, :webapp),
+        :target_subdir => 'css',
+        :source_dir => BuildrPlus::Less.default_less_path,
+        :source_pattern => '**/[^_]*.less'
+      }.merge(options)
+
+      source_dir = project._(params[:source_dir])
+      source_pattern = params[:source_pattern]
+      target_dir = params[:target_dir]
+
+      files = FileList["#{source_dir}/#{source_pattern}"]
+
+      if files.size > 0
+        desc 'Preprocess Less files'
+        compile_task = project.task('lessc' => [files]) do
+          command = []
+          command << 'yarn'
+          command << 'run'
+          command << 'lessc'
+          command << '--'
+          command << '--no-js' unless params[:js]
+          command << "--strict-math=#{!!params[:strict_math] ? 'on' : 'off'}"
+          command << "--strict-units=#{!!params[:strict_units] ? 'on' : 'off'}"
+
+          if params[:optimize]
+            command << '--compress'
+            command << '--clean-css'
+          end
+
+          target_subdir = params[:target_subdir].nil? ? '' : "#{params[:target_subdir]}/"
+
+          puts 'Compiling Less'
+          files.each do |f|
+            sh "#{command.join(' ')} #{f} #{target_dir}/#{target_subdir}#{Buildr::Util.relative_path(f, source_dir)[0...-5]}.css"
+          end
+          touch target_dir
+        end
+
+        project.task(':generate:all' => [compile_task])
+
+        project.assets.paths << project.file(target_dir => [compile_task])
+        target_dir
+      else
+        nil
+      end
+    end
   end
 
   f.enhance(:ProjectExtension) do
@@ -38,13 +91,9 @@ BuildrPlus::FeatureManager.feature(:less) do |f|
       File.exist?(less_path)
     end
 
-    first_time do
-      require 'buildr_plus/patches/lessc'
-    end
-
     before_define do |project|
       if project.lessc_required?
-        define_lessc_task(project, BuildrPlus::Less.options)
+        BuildrPlus::Less.define_lessc_task(project, BuildrPlus::Less.options)
         task(':domgen:all').enhance(["#{project.name}:lessc"])
         if project.ipr?
           p = if BuildrPlus::Roles.project_with_role?(:server)
