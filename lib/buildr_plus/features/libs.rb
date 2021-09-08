120a12b10480c2dd177f07e8ac3bdb6a80a48306
Peter Donald <peter@realityforge.org>
2021-09-08 14:57:10 +1000

    Bump akasha version

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -331,7 +331,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def akasha
-      %w(org.realityforge.akasha:akasha-gwt:jar:0.22)
+      %w(org.realityforge.akasha:akasha-gwt:jar:0.23)
     end
 
     def react4j_version
