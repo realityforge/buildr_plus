79b9249b3d4ea95f5448ac6c9beebd4a635ec674
Peter Donald <peter@realityforge.org>
2024-02-02 09:06:38 +1100

    Bump bazel version

--- lib/buildr_plus/features/bazel.rb
+++ lib/buildr_plus/features/bazel.rb
@@ -15,7 +15,7 @@
 BuildrPlus::FeatureManager.feature(:bazel) do |f|
   f.enhance(:Config) do
     def bazel_version
-      @bazel_version ||= '6.4.0'
+      @bazel_version ||= '6.5.0'
     end
 
     attr_writer :bazel_version
