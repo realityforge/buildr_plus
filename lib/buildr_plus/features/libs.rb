8d8f0cc9e223cc3b561b267d49dbe0c01fae2a9f
Peter Donald <peter@realityforge.org>
2025-01-15 13:43:42 +1100

    Bump grim version

--- lib/buildr_plus/features/libs.rb
+++ lib/buildr_plus/features/libs.rb
@@ -302,7 +302,7 @@ BuildrPlus::FeatureManager.feature(:libs) do |f|
     end
 
     def grim_annotations
-      %w(org.realityforge.grim:grim-annotations:jar:0.06)
+      %w(org.realityforge.grim:grim-annotations:jar:0.09)
     end
 
     def router_fu_version
