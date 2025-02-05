335921a71398f6f2d572f0634c0d48e1c3cb80e6
Peter Donald <peter@realityforge.org>
2025-02-05 11:29:44 +1100

    Start to move towards separating test configuration into a file

--- lib/buildr_plus/features/testng.rb
+++ lib/buildr_plus/features/testng.rb
@@ -21,25 +21,7 @@ BuildrPlus::FeatureManager.feature(:testng) do |f|
 
       if BuildrPlus::FeatureManager.activated?(:db) && project_descriptor && project_descriptor.in_any_role?([:library, :server, :sync_model, :model, :model_qa])
         if BuildrPlus::FeatureManager.activated?(:dbt)
-          BuildrPlus::Config.load_application_config! if BuildrPlus::FeatureManager.activated?(:config)
-          Dbt.repository.load_configuration_data
-
-          Dbt.database_keys.each do |database_key|
-            next if BuildrPlus::Dbt.manual_testing_only_database?(database_key)
-
-            database = Dbt.configuration_for_key(database_key, :test)
-            jdbc_url = database.build_jdbc_url(:credentials_inline => true)
-
-            prefix = database_key
-            if Dbt::Config.default_database?(database_key)
-              default_testng_args << "-Dtest.db.url=#{jdbc_url}"
-              default_testng_args << "-Dtest.db.name=#{database.catalog_name}"
-              prefix = project.root_project.name.to_s
-            end
-
-            default_testng_args << "-D#{prefix}.test.db.url=#{jdbc_url}"
-            default_testng_args << "-D#{prefix}.test.db.name=#{database.catalog_name}"
-          end
+          default_testng_args << "-Dtest.db.property_file=#{project.root_project._('generated/buildr_plus/config/testng.properties')}"
         end
       end
 
@@ -72,7 +54,49 @@ BuildrPlus::FeatureManager.feature(:testng) do |f|
       default_testng_args
     end
   end
+
   f.enhance(:ProjectExtension) do
+    after_define do |project|
+
+      if project.ipr?
+        project.task(':generate:all' => ['config:emit_test_properties']) if BuildrPlus::FeatureManager.activated?(:generate)
+
+        desc 'Generate properties files used in the testing process'
+        project.task(':config:emit_test_properties') do
+
+          filename = project._('generated/buildr_plus/config/testng.properties')
+
+          if BuildrPlus::FeatureManager.activated?(:db)
+            if BuildrPlus::FeatureManager.activated?(:dbt)
+              BuildrPlus::Config.load_application_config! if BuildrPlus::FeatureManager.activated?(:config)
+              Dbt.repository.load_configuration_data
+
+              trace("Generating testng properties in #{filename}")
+              File.open(filename, 'wb') do |file|
+                file.write "# DO NOT EDIT: File is auto-generated\n"
+
+                Dbt.database_keys.each do |database_key|
+                  next if BuildrPlus::Dbt.manual_testing_only_database?(database_key)
+
+                  database = Dbt.configuration_for_key(database_key, :test)
+                  jdbc_url = database.build_jdbc_url(:credentials_inline => true)
+
+                  prefix = database_key
+                  if Dbt::Config.default_database?(database_key)
+                    file.write "test.db.url=#{jdbc_url}\n"
+                    file.write "test.db.name=#{database.catalog_name}\n"
+                    prefix = project.root_project.name.to_s
+                  end
+
+                  file.write "#{prefix}.test.db.url=#{jdbc_url}\n"
+                  file.write "#{prefix}.test.db.name=#{database.catalog_name}\n"
+                end
+              end
+            end
+          end
+        end
+      end
+    end
 
     attr_writer :idea_testng_configuration_created
 
