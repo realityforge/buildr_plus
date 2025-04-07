04b166544804ab3205216889197e8ffc9d7bcdf0
Peter Donald <peter@realityforge.org>
2025-04-07 19:00:38 +1000

    Remove support for gwt_rpc

--- lib/buildr_plus/features/deps.rb
+++ lib/buildr_plus/features/deps.rb
@@ -89,7 +89,6 @@ BuildrPlus::FeatureManager.feature(:deps => [:libs]) do |f|
       end
 
       generators << [:robots] if BuildrPlus::Artifacts.war?
-      generators << [:gwt_rpc_shared, :gwt_rpc_server] if BuildrPlus::FeatureManager.activated?(:gwt)
       generators << [:imit_shared, :imit_server_service, :imit_server_qa] if BuildrPlus::FeatureManager.activated?(:replicant)
       generators << [:action_server] if BuildrPlus::FeatureManager.activated?(:action)
 
@@ -129,7 +128,6 @@ BuildrPlus::FeatureManager.feature(:deps => [:libs]) do |f|
         generators << [:imit_server_entity_replication] if BuildrPlus::FeatureManager.activated?(:replicant)
       end
 
-      generators << [:gwt_rpc_shared, :gwt_rpc_server] if BuildrPlus::FeatureManager.activated?(:gwt)
       generators << [:imit_shared, :imit_server_service, :imit_server_qa] if BuildrPlus::FeatureManager.activated?(:replicant)
 
       if BuildrPlus::FeatureManager.activated?(:sync)
@@ -162,7 +160,7 @@ BuildrPlus::FeatureManager.feature(:deps => [:libs]) do |f|
     end
 
     def gwt_generators
-      generators = [:ce_data_types, :gwt, :gwt_rpc_shared, :gwt_rpc_client_service, :gwt_client_jso, :gwt_client_module, :gwt_client_gwt_model_module]
+      generators = [:ce_data_types, :gwt, :gwt_rpc_client_service, :gwt_client_jso, :gwt_client_module, :gwt_client_gwt_model_module]
       generators += [:keycloak_gwt_jso] if BuildrPlus::FeatureManager.activated?(:keycloak)
       generators += [:arez_entity] if BuildrPlus::FeatureManager.activated?(:arez)
       if BuildrPlus::FeatureManager.activated?(:replicant)
@@ -189,8 +187,7 @@ BuildrPlus::FeatureManager.feature(:deps => [:libs]) do |f|
     end
 
     def gwt_qa_test_generators
-      generators = [:gwt_rpc_test_qa_external]
-      generators += [:gwt_client_test_jso_qa_support]
+      generators = [:gwt_client_test_jso_qa_support]
       generators += [:imit_client_test_qa_external] if BuildrPlus::FeatureManager.activated?(:replicant)
       generators += [:keycloak_gwt_test_qa] if BuildrPlus::FeatureManager.activated?(:keycloak)
       generators += [:arez_test_qa_external] if BuildrPlus::FeatureManager.activated?(:arez)
