8971dcf3de3b4767f56901d4ef81cb3a0ed3fe0a
Peter Donald <peter@realityforge.org>
2026-02-12 17:11:31 +1100

    Support multiple roots for keep files

--- lib/buildr_plus/features/generate.rb
+++ lib/buildr_plus/features/generate.rb
@@ -41,6 +41,12 @@ BuildrPlus::FeatureManager.feature(:generate) do |f|
       @inline_generated_source.nil? ? false : !!@inline_generated_source
     end
 
+    attr_writer :generated_source_bases
+
+    def generated_source_bases
+      @generated_source_bases.nil? ? [File.expand_path(project._('src'))] : @generated_source_bases
+    end
+
     def extra_keep_file_names
       @extra_keep_file_names ||= []
     end
@@ -54,16 +60,14 @@ BuildrPlus::FeatureManager.feature(:generate) do |f|
     end
 
     def keep_file_names
-      if File.exist?(keep_filename_registry)
-        base = File.expand_path(project._('src'))
-        IO.read(keep_filename_registry).split("\n").collect { |f| "#{base}/#{f}" }
-      else
-        []
+      results = []
+      generated_source_bases.collect do |base|
+        keep_filename_registry = base + "/keep_files.txt"
+        if File.exist?(keep_filename_registry)
+          results << IO.read(keep_filename_registry).split("\n").collect { |f| "#{base}/#{f}" }
+        end
       end
-    end
-
-    def keep_filename_registry
-      File.expand_path(project._('src')) + "/keep_files.txt"
+      results.flatten
     end
 
     first_time do
@@ -77,23 +81,24 @@ BuildrPlus::FeatureManager.feature(:generate) do |f|
       desc 'Collect the files that will be kept and not cleaned by domgen clean processes'
       t = project.task 'collect_keep_files' do
         if project.inline_generated_source?
-          target_dir = File.expand_path(project._('src'))
-          files = []
-          Dir["#{target_dir}/**/*"].each do |file_name|
-            if !File.directory?(file_name) && !IO.read(file_name).include?('DO NOT EDIT: File is auto-generated')
-              files << ::Buildr::Util.relative_path(file_name, target_dir)
-            end
-          end
-          files += project.extra_keep_file_names
-          new_content = files.sort.join("\n") + "\n"
-
           keep_files_regenerated = false
 
-          existing = IO.read("#{target_dir}/keep_files.txt") rescue ''
-          if existing != new_content
-            FileUtils.mkdir_p target_dir
-            IO.write("#{target_dir}/keep_files.txt", new_content)
-            keep_files_regenerated = true
+          project.generated_source_bases.collect do |target_dir|
+            files = []
+            Dir["#{target_dir}/**/*"].each do |file_name|
+              if !File.directory?(file_name) && !IO.read(file_name).include?('DO NOT EDIT: File is auto-generated')
+                files << ::Buildr::Util.relative_path(file_name, target_dir)
+              end
+            end
+            files += project.extra_keep_file_names
+            new_content = files.sort.join("\n") + "\n"
+
+            existing = IO.read("#{target_dir}/keep_files.txt") rescue ''
+            if existing != new_content
+              FileUtils.mkdir_p target_dir
+              IO.write("#{target_dir}/keep_files.txt", new_content)
+              keep_files_regenerated = true
+            end
           end
 
           Domgen.error("Regenerated keep files, aborting build to ensure no files are accidentally deleted. Please try build again") if keep_files_regenerated
